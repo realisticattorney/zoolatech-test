ed802acebd7f516e37d20674233a2de75d85d6fb
realisticattorney <g.axl.aquila@gmail.com>
2022-05-25 19:59:40 -0300

    Complete up to 1.3

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,17 +1,29 @@
-import "./index.css";
+import './index.css';
 
 export default function Tree() {
   return (
     <div className="tree">
-      <p>root</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
-      </p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+      <ul>
+        <li>
+          root
+          <ul>
+            <li>ant</li>
+            <li>
+              bear
+              <ul>
+                <li>cat</li>
+                <li>
+                  dog
+                  <ul>
+                    <li>elephant</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>frog</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 }

50fa85fbb0ef9e5d617d7243f0949bb8bf4c8b2f
realisticattorney <g.axl.aquila@gmail.com>
2022-05-25 20:50:45 -0300

    Complete bonus 1.6. Skip bonus 1.5

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,16 +1,27 @@
 import './index.css';
 let node = require('./data.json');
 
-export default function Tree({ name, children }) {
-  let data = name ? { name, children } : node; //if name is not null, then it's a child node, else it's the root node
+export default function Tree({ name, children, dotsCount }) {
+  let data = name ? { name, children, dotsCount } : node; //if name is not null, then it's a child node, else it's the root node
+  let dotsCountAcc = dotsCount || 0; //dotsCountAcc is the number of dots that have been added to the current node
   return (
     <div className={`${name ? '' : 'tree'}`}>
       <ul>
         <li>
-          {name ? name : node.name}
+          {name
+            ? name
+                .substring(0, 1)
+                .concat('.'.repeat(dotsCount))
+                .concat(name.substring(1))
+            : node.name}
           {data.children.length > 0 &&
             data.children.map(({ name, children }) => (
-              <Tree key={name} name={name} children={children} />
+              <Tree
+                key={name}
+                name={name}
+                children={children}
+                dotsCount={dotsCountAcc + 1}
+              />
             ))}
         </li>
       </ul>
